2c60aaa22bf6b317db16d520da2c237e22dc2f9e
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2021-11-17 17:22:19 +0200

    Update client.cs

--- client.cs
+++ client.cs
@@ -3,27 +3,19 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace dote
 {
     class Program
     {
-static void Connect(String server, String message)
+static string GetMessage(TcpClient client, String message)
 {
+    String responseData = String.Empty;
   try
   {
-    // Create a TcpClient.
-    // Note, for this client to work you need to have a TcpServer
-    // connected to the same address as specified by the server, port
-    // combination.
-    Int32 port = 5600;
-    TcpClient client = new TcpClient(server, port);
-
     // Translate the passed message into ASCII and store it as a Byte array.
     Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-    // Get a client stream for reading and writing.
-   //  Stream stream = client.GetStream();
 
     NetworkStream stream = client.GetStream();
 
@@ -38,16 +30,16 @@ static void Connect(String server, String message)
     data = new Byte[256];
 
     // String to store the response ASCII representation.
-    String responseData = String.Empty;
+    
 
     // Read the first batch of the TcpServer response bytes.
     Int32 bytes = stream.Read(data, 0, data.Length);
     responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-    Console.WriteLine("Received: {0}", responseData);
-
+    
     // Close everything.
     stream.Close();
-    client.Close();
+
+
   }
   catch (ArgumentNullException e)
   {
@@ -56,19 +48,31 @@ static void Connect(String server, String message)
   catch (SocketException e)
   {
     Console.WriteLine("SocketException: {0}", e);
-  }
+  }    
 
-  Console.WriteLine("\n Press Enter to continue...");
-  Console.Read();
+   return responseData;
+
+  
 }
-
-
 
       public static void Main ()
         {
             string server = "127.0.0.1";
-            string message = "{\"hello\" : \"world\"}";
-            Connect(server,message);
+            var port = 5600;
+            
+            int count = 0 ;
+            while(count < 1<<24)
+            {
+                count++;
+                string message = string.Format("{0}",count);                
+                using (var client = new TcpClient(server,port))
+                {
+                    var ret_message = GetMessage(client,message);
+                    Console.WriteLine(ret_message);
+                }       
+                Thread.Sleep(500);
+            }            
         }
     }
 }
+
