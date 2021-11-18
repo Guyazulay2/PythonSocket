a29b85064b105583da687f05288198575f1dbf3e
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2021-11-18 11:16:00 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,25 +3,32 @@ from threading import Thread
 from pprint import pprint
 import json
 
-HOST = '127.0.0.1'
-PORT = 8080
+HOST = '0.0.0.0'
+PORT = 5600
 
 class SingleTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request.recv(1024)
         text = data.decode('utf-8')
-        pprint(json.loads(text))
-        for key in json.loads(text):
-            pprint(json.loads(text)[key])
-        self.request.send(bytes(json.dumps({"status":"success!"}), 'UTF-8'))
+        print(text)
+        datatosend = ''
+        with open("data.json",'r') as f:
+            j = json.load(f)
+            datatosend = json.dumps(j)
+
+            
+        self.request.send(bytes(datatosend, 'UTF-8'))
         self.request.close()
 
+        
 class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     daemon_threads = True
     allow_reuse_address = True
 
     def __init__(self, server_address, RequestHandlerClass):
         socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass)
+
+
 
 if __name__ == "__main__":
     server = SimpleServer((HOST, PORT), SingleTCPHandler)
